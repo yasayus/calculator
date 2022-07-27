674e1b1a7987dfef5379ec88d0748571e6a49cc1
yasayus <soyusuf@proton.me>
2022-07-27 13:28:13 +0200

    Calculator
    my simple calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.querySelector(".display");
+const buttons = Array.from(document.getElementsByTagName("button"));
+
+buttons.map((button) => {
+	button.addEventListener("click", (event) => {
+		switch (event.target.innerText) {
+			case "C":
+				display.innerText = "";
+				break;
+			case "=":
+				try {
+					display.innerText = eval(display.innerText);
+				} catch {
+					display.innerText = "Error";
+				}
+				break;
+			default:
+				display.innerText += event.target.innerText;
+		}
+	});
+});
